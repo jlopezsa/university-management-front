a4e84eb71e86b2994585cf1a017b8d2cc909b6df
Julian Lopez <jlopezsa@gmail.com>
2022-06-30 17:50:48 -0500

    feat: create services to conect to API Program

--- src/services/program.js
+++ src/services/program.js
@@ -1,5 +1,16 @@
 const API_URL = 'http://localhost:8080';
 
+export async function getProgramByName(name) {
+  const { programName } = name;
+  try {
+    const response = await fetch (`${API_URL}/api/proram/search?name=${programName}`);
+    const program = await response.json();
+    return program;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
 export async function registerProgram(program) {
   const payload = {
     method: 'POST',
