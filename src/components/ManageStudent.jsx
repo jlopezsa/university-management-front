a62f72f38d64dd2482b7052f3ba061585940f9b2
Julian Lopez <jlopezsa@gmail.com>
2022-06-30 09:33:53 -0500

    feat: create form to register a student

--- src/components/ManageStudent.jsx
+++ src/components/ManageStudent.jsx
@@ -1,6 +1,34 @@
+import { useState } from 'react';
+import { Input, Button } from './AtomicComponents';
 function ManageStudent() {
+
+  const [ student, setStudent ] = useState({});
+
+  const handleChange = (e) => {
+    setStudent({
+      ...student,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleCreateStudent = (e) => {
+    e.preventDefault();
+    console.log('Datos del estudiante: ', student);
+  }
+
   return (
-    <div>ManageStudent</div>
+    <div>
+      <h2>Administración de datos del esudiante</h2>
+      <h4>Ingrese los siguientes datos del estudiante</h4>
+      <form action="">
+        <Input onChange={handleChange} type="text" name="userId" placeholder="Id" />
+        <Input onChange={handleChange} type="number" name="userCedula" placeholder="Cedula" />
+        <Input onChange={handleChange} type="text" name="userName" placeholder="Nombre" />
+        <Input onChange={handleChange} type="email" name="userEmail" placeholder="Email" />
+        <Input onChange={handleChange} type="tel" name="userPhone" placeholder="Telefono" />
+        <Button onClick={handleCreateStudent}>Registrar estudiante</Button>
+      </form>
+    </div>
   )
 }
 
